5ef41cc65570b98764c7af1e5ef2b124ec7faba3
Thomas Guillet <guillet.thomas@gmail.com>
2018-02-16 23:15:27 +0100

    Synchronous additions

--- enrich.js
+++ enrich.js
@@ -6,63 +6,66 @@ const fs = Promise.promisifyAll(require('fs'))
 const yaml = require('js-yaml')
 
 function YAMLtoJSON (p) {
-  return fs.readFileAsync(p)
-    .then(yaml.safeLoad)
-    .then(organisme => {
-      organisme.horaires = organisme.horaires || (organisme['accueil physique'] && organisme['accueil physique'].horaires)
+  const content = fs.readFileSync(p)
+  let organisme = yaml.safeLoad(content)
+  organisme.horaires = organisme.horaires || (organisme['accueil physique'] && organisme['accueil physique'].horaires)
 
-      const geoJson = {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [null, null]
-        },
-        properties: organisme
-      }
+  const geoJson = {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [null, null]
+    },
+    properties: organisme
+  }
 
-      return {
-        path: p,
-        json: geoJson
-      }
-    })
+  return {
+    path: p,
+    json: geoJson
+  }
+}
+
+function appendOrganisme (holder, key, value) {
+  holder.organismes[key] = holder.organismes[key] || []
+  holder.organismes[key].push(value)
 }
 
 function addOrganismes (folder, dataset) {
-  return Promise.map(fs.readdirAsync(folder), departementFolder => {
+  let additionCount = 0
+  fs.readdirSync(folder).map(departementFolder => {
     const departementPath = path.join(folder, departementFolder)
 
-    return Promise.map(fs.readdirAsync(departementPath), organismeFile => {
+    fs.readdirSync(departementPath).map(organismeFile => {
+      additionCount = additionCount + 1
       const organismePath = path.join(departementPath, organismeFile)
-      return YAMLtoJSON(organismePath)
-        .then(organisme => {
-          const props = organisme.json.properties
-          dataset.organismes[props.pivotLocal] = dataset.organismes[props.pivotLocal] || []
-          dataset.organismes[props.pivotLocal].push(organisme.json)
+      const organisme = YAMLtoJSON(organismePath)
 
-          props.zonage.communes.forEach(communesLabel => {
-            const communeId = communesLabel.slice(0, 5)
+      const props = organisme.json.properties
+      dataset.organismes[props.id] = dataset.organismes[props.pivotLocal] || []
+      dataset.organismes[props.id] = organisme.json
 
-            let commune = dataset.communes[communeId]
-            if (!commune) {
-              return
-            }
+      props.zonage.communes.forEach(communesLabel => {
+        const communeId = communesLabel.slice(0, 5)
 
-            commune.organismes[props.pivotLocal] = commune.organismes[props.pivotLocal] || []
-            commune.organismes[props.pivotLocal].push(organisme.json)
-          })
+        let commune = dataset.communes[communeId]
+        if (!commune) {
+          return
+        }
 
-          if (!dataset.departements[departementFolder]) {
-            return dataset
-          }
+        appendOrganisme(commune, props.pivotLocal, props.id)
+      })
 
-          let departement = dataset.departements[departementFolder]
-          departement.organismes[props.pivotLocal] = departement.organismes[props.pivotLocal] || []
-          departement.organismes[props.pivotLocal].push(organisme.json)
+      if (!dataset.departements[departementFolder]) {
+        return
+      }
 
-          return dataset
-        })
+      appendOrganisme(dataset.departements[departementFolder], props.pivotLocal, organisme.json)
     })
-  }).then(() => dataset)
+  })
+
+  console.log(`Added ${additionCount} organismes.`)
+
+  return dataset
 }
 
 module.exports = addOrganismes
--- test/enrich.test.js
+++ test/enrich.test.js
@@ -4,7 +4,7 @@ const enrich = require('../enrich')
 describe('enrich', function () {
   let dataset
 
-  before((done) => {
+  before(() => {
     dataset = {
       communes: {
         '29011': {
@@ -18,20 +18,22 @@ describe('enrich', function () {
       },
       organismes: {}
     }
-    enrich('./data', dataset).then(() => done())
+    enrich('./data', dataset)
   })
 
-  it('adds organisme', () => {
+  it('adds organisme in dataset', () => {
     assert.equal(Object.keys(dataset.organismes).length, 1)
+    assert.ok(dataset.organismes.cdas_brest_bellevue.properties, 1)
   })
 
-  it('adds an organisme in commune', () => {
+  it('adds the organisme ID in commune', () => {
     assert.equal(Object.keys(dataset.communes['29011'].organismes).length, 1)
-    assert.equal(Object.keys(dataset.communes['29011'].organismes.cdas).length, 1)
+    assert.equal(dataset.communes['29011'].organismes.cdas.length, 1)
+    assert.equal(dataset.communes['29011'].organismes.cdas[0], 'cdas_brest_bellevue')
   })
 
   it('adds an organisme in departement', () => {
     assert.equal(Object.keys(dataset.departements['29'].organismes).length, 1)
-    assert.equal(Object.keys(dataset.departements['29'].organismes.cdas).length, 1)
+    assert.ok(dataset.departements['29'].organismes.cdas[0].properties)
   })
 })
